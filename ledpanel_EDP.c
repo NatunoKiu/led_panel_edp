b6c6caa20f14b45d24820b3a598cb1d51d3228f0
NatunoKiu <natukiu7272@gmail.com>
2024-11-14 13:37:53 +0900

    関数の定義の順番を変更。

--- ledpanel_EDP.c
+++ ledpanel_EDP.c
@@ -17,22 +17,6 @@
 #define WS2812_PIN 1
 #endif
 
-void printChar(char chr)
-{
-    uint8_t chrNum = chr - 48;
-    uint8_t charData[8];
-
-    // 0 ~ 9 の読み込み処理
-    if (chrNum <= 9)
-    {
-        for (int i = 0; i < 8; ++i)
-        {
-            // 対応関係：'0' = 48 = index:(1+26)*8-1
-            charData[i] = fontReversed[chrNum + (1+26)*8-1 + i];
-        }
-    }
-}
-
 static inline void put_pixel(uint32_t pixel_grb)
 {
     pio_sm_put_blocking(pio0, 0, pixel_grb << 8u);
@@ -43,6 +27,22 @@ static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b)
     return ((uint32_t)(r) << 8) |
            ((uint32_t)(g) << 16) |
            (uint32_t)(b);
+}
+
+void printChar(char chr)
+{
+    uint8_t chrNum = chr - 48;
+    uint8_t charData[8];
+                           
+    // 0 ~ 9 の読み込み処理
+    if (chrNum <= 9)
+    {
+        for (int i = 0; i < 8; ++i)
+        {
+            // 対応関係：'0' = 48 = index:(1+26)*8-1
+            charData[i] = fontReversed[chrNum + (1+26)*8-1 + i];
+        }
+    }
 }
 
 int main()
